2f29c053a0a1d50fc25b125dfb8eef55a6e3d2b9
Muriellehxh <dmui49@gmail.com>
2021-11-16 17:07:50 -0500

    ch10

--- notes_ch10.py
+++ notes_ch10.py
@@ -12,10 +12,35 @@ class Velo:
 		self.color = "bleu"  # =  attribut public
 		self.position = 0
 
-	def avance(self):
+	def avance(self):  # methode
 		self.position += 1
+
+	@staticmethod
+	def infovalue():
+		print("mot")
+
+	def __add__(self, other):
+		return self.position + other.position
+
+	@color.setter
+	def color(self, value):
+		if type(value) == str:
+			self.__color = value
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
 	velo = Velo("rouge", 10) # reference a def ()
 
+	velo1 = Velo()
+	velo2 = Velo("rouge")
+	#si on veut additioner v1 et v2
+
+	print(velo1 + velo2)  # while using def __add__
+
+   # error in yellow??
