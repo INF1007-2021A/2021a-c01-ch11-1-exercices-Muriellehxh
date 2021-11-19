59da951fcc5b6fcb9c6453fff1e1afb1b5693ad0
Muriellehxh <dmui49@gmail.com>
2021-11-19 15:16:51 -0500

    ch10

--- notes_ch10.py
+++ notes_ch10.py
@@ -1,4 +1,3 @@
-
 # attribut = on peut y accéder à exterieur
 #   comment accéder
 
@@ -7,35 +6,44 @@
 
 
 class Velo:
-	def __init__(self, color, position):  # on donne valeur en parametre
-		self.__color = "bleu"  # =  attribut privé (empeche modification à exterieur)
-		self.color = "bleu"  # =  attribut public
-		self.position = 0
+    def __init__(self, color, position):  # on donne valeur en parametre
+        self.__color = "bleu"  # =  attribut privé (empeche modification à exterieur)
+        self.color = "bleu"  # =  attribut public
+        self.position = 0
 
-	def avance(self):  # methode
-		self.position += 1
+    def avance(self):  # methode
+        self.position += 1
 
-	@staticmethod
-	def infovalue():
-		print("mot")
+    @staticmethod
+    def infovalue():
+        print("mot")
 
-	def __add__(self, other):
-		return self.position + other.position
+    def __add__(self, other):
+        return self.position + other.position
 
-	@color.setter
-	def color(self, value):
-		if type(value) == str:
-			self.__color = value
+    @color.setter
+    def color(self, value):
+        if type(value) == str:
+            self.__color = value
 
 
+class Personne():
+    def __init__(self, nom, age):  # method with attributes in () en parametres
+        self.name = nom
+        self.age = age
+
+
+nom_instance = Personne("Mario", "2") # class name ( attribute values )
+
+print(x.name)
 
 if __name__ == "__main__":
-	velo = Velo("rouge", 10) # reference a def ()
+    velo = Velo("rouge", 10)  # reference a def ()
 
-	velo1 = Velo()
-	velo2 = Velo("rouge")
-	#si on veut additioner v1 et v2
+    velo1 = Velo()
+    velo2 = Velo("rouge")
+    # si on veut additioner v1 et v2
 
-	print(velo1 + velo2)  # while using def __add__
+    print(velo1 + velo2)  # while using def __add__
 
-   # error in yellow??
+# error in yellow??
